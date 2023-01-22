1e0ae1b608bafce69b70a4e9b078a59e544d84fa
DESKTOP-CHS5Q34\Alberto <albertomarmad@gmail.com>
2023-01-22 15:26:27 -0600

    dev branch init

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app.include_router(api_router, prefix=settings.API_STR)
 async def satrtUp():
     # db.addDataFromCSV()
     pass
-
+#
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
