129078f0452a5341f2ff32939d2c5279de064380
GillMarMad <gillmarmad@gmail.com>
2023-01-31 02:25:44 -0600

    Server runnin on Ubuntu

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,8 @@
 from pydantic import BaseSettings
 class Settings(BaseSettings):
     POSTGRES_USER: str = "marpo"
-    POSTGRES_PASSWORD = "marpo9711251"
-    POSTGRES_SERVER: str = "localhost"
+    POSTGRES_PASSWORD = "marpodb2023"
+    POSTGRES_SERVER: str = "192.168.1.92"
     POSTGRES_PORT: str = "5432"
     POSTGRES_DB: str = "marpo_db"
     PROJECT_NAME: str = "Marpo System"
