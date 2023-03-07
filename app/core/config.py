79d06624ab99e19f798a1aceae6a870f7088b303
GillMarMad <gillmarmad@gmail.com>
2023-03-07 12:58:04 -0600

    new ip set

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     POSTGRES_USER: str = "marpo"
     POSTGRES_PASSWORD = "marpodb2023"
-    POSTGRES_SERVER: str = "192.168.1.92"
+    POSTGRES_SERVER: str = "192.168.1.86"
     POSTGRES_PORT: str = "5432"
     POSTGRES_DB: str = "marpo_db"
     PROJECT_NAME: str = "Marpo System"
